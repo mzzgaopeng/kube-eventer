c7f4cdc056fa9d2e3c401ec035c49ab4f8331f5e
root <penggao@harmonycloud.cn>
2020-04-14 18:40:55 +0800

    driver.go add event time

--- sinks/kafka/driver.go
+++ sinks/kafka/driver.go
@@ -50,7 +50,11 @@ type kafkaSink struct {
 
 
 func eventToPoint(event *kube_api.Event, clusterName string) (*KafkaSinkPoint, error) {
-
+	zeroTime := time.Time{}
+	if event.EventTime.Time != zeroTime {
+		event.FirstTimestamp = event.CreationTimestamp
+		event.LastTimestamp = event.CreationTimestamp
+	}
 	sinkPoint := KafkaSinkPoint{
 		EventKind:            event.InvolvedObject.Kind,
 		EventMessage:         event.Message,
